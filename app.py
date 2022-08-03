c3afc820793272a3b622d2c21941d070bea5fe14
Phanuphong <mind.sathitkul@gmail.com>
2022-08-03 15:57:21 +0700

    make it better

---
+++ app.py
@@ -0,0 +1,22 @@
+import json
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/webhook", methods=["POST"])
+def callback():
+    request_json = request.get_json(silent=True, force=True)
+    try:
+        if request_json["events"][0]["message"]["type"] == "text":
+            headers = dict(request.headers)
+            headers.update({"Host": "dialogflow.cloud.google.com"})
+            requests.post("https://dialogflow.cloud.google.com/v1/integrations/line/webhook/feee7085-d5e1-4afe-a3b3-593afc0ddf56",
+                          data=request.data, headers=headers)
+    except:
+        None
+
+
+if __name__ == "main":
+    app.run()
